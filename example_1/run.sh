5998b18f1f68e73d01ca479c14d88f8a94b1dbb3
Yanis Guenane <yguenane@gmail.com>
2014-07-29 17:28:36 -0400

    Example1: update hiera configuration files

--- example_1/run.sh
+++ example_1/run.sh
@@ -11,4 +11,13 @@ echo "Getting the modules..."
 PUPPETFILE=./Puppetfile PUPPETFILE_DIR=modules r10k --verbose 3 puppetfile install
 echo "Creating default.pp..."
 cp example.pp manifests/default.pp
+
+# Disable include ::sudo for Vagrant purpose
+# Since it automaticailly set Default requiretty
+sed -i 's/include ::sudo/#include :sudo/' modules/profile/manifests/base.pp
+
+# Remove unmet symlink in account
+# puppet module
+rm modules/account/.travis.yml
+
 echo "Everything is done !"
